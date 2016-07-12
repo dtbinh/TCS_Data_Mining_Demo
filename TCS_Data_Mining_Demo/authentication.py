fbf145063cc213cd8190668f33096304d887d546
priyabha <priyabha@usc.edu>
2016-07-12 10:28:09 +0530

    added demo.py with db functionalities

--- TCS_Data_Mining_Demo/authentication.py
+++ TCS_Data_Mining_Demo/authentication.py
@@ -1,7 +1,7 @@
 import tweepy
 
-consumer_key = "NM3ODIGJ7iDTwISr7mRdIe60Y" # To get a key go to apps.twitter.com 
-consumer_secret = "Mu6rMnPHeLNkNP1nfLa3kChtDDqiH4e28UISS18h4GPdTojkhi"
+consumer_key = "LIBzsGBt3vlY1mUbpM8vJjbq0" # To get a key go to apps.twitter.com 
+consumer_secret = "HF1IKSattNKhHH69fvqfD2ShCnGISlHnkqPS8dZuok6G8aq16Y"
 
 auth = tweepy.OAuthHandler(consumer_key = consumer_key, consumer_secret = consumer_secret)
 api = tweepy.API(auth)
@@ -9,26 +9,18 @@ api = tweepy.API(auth)
 #@param - tweet 
 # Prints the screen name, username, date of tweet and text
 def print_tweet(tweet):
-	print(tweet.user.screen_name + " " + tweet.user.name + " " + str(tweet.created_at))
+	#print(tweet.user.screen_name + " " + tweet.user.name + " " + str(tweet.created_at))
 	print((tweet.text).encode('utf-8')) #unicode
 
 #Default 15 results unless specified in count
-# results = api.search(q="happy", count = 100, result_type = "recent")
-
-# print(len(results)) 
-
-# for tweet in results:
-# 	print_tweet(tweet)
-
 
 #dir(tweet) -->lists everything inside the tweet object
 
-#Using curor for pagination
+#Using cursor for pagination
 
 print("--------------------------------------")
 results2 = []
-searchWord = "iphone7"
+searchWord = "iPhone7"
 
 for tweet in tweepy.Cursor(api.search, q=searchWord, count = 1000).items():
-	print((tweet.text).encode('utf-8'))
-	results2.append(tweet)
+	print_tweet(tweet)
--- TCS_Data_Mining_Demo/demo.py
+++ TCS_Data_Mining_Demo/demo.py
@@ -2,10 +2,30 @@
 import tweepy
 import pymongo
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import matplotlib as plt
-import ipwidgets as wgt
+#import ipwidgets as wgt
 from datetime import datetime
+
+#listener
+class MyStreamListener(tweepy.StreamListener):
+
+	counter = 0
+
+	def __init__(self, max_tweets = 1000, *args, **kwargs):
+		self.max_tweets = max_tweets
+		self.counter = 0
+		super().__init__(*args, **kwargs)
+
+		def on_connect(self):
+			self.counter = 0
+			self.start_time = datetime.now()
+
+	def on_status(self, status):
+		self.counter += 1
+
+		#store tweet to mongoDB
+		col.insert_one(status._json)
 
 consumer_key = "LIBzsGBt3vlY1mUbpM8vJjbq0" # To get a key go to apps.twitter.com 
 consumer_secret = "HF1IKSattNKhHH69fvqfD2ShCnGISlHnkqPS8dZuok6G8aq16Y"
@@ -19,5 +39,22 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #collection
-col = pymongo.MongoClient() #db name and collection name
-col.count()
+col = pymongo.MongoClient()["tweets"]["sentimentAnalysis"] #db name and collection name
+print(col.count())
+
+myStreamListener = MyStreamListener(max_tweets=1000)
+myStream = tweepy.Stream(api.auth, listener = myStreamListener)
+
+#keywords for which we will find tweets
+keywords = ["iphone6", "iphone7"]
+
+#starting a filter
+
+for error_counter in range(20):
+	try: 
+		myStream.filter(track = keywords)
+		print("Tweets collected: %s" % myStream.listener.counter)
+		print("Total tweets in collection: %s" %col.count())
+		break
+	except:
+		print("ERROR # %s" % (error_counter + 1))
